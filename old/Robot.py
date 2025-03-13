7217682ce4bad8ef18b6bf92cdca2fbec22b063a
Sam Adams <sammyadams04@gmail.com>
2025-03-13 19:35:48 -0400

    OPENING AND CLOSING OF GRIPPER WITH MOVEMENT

--- old/Robot.py
+++ old/Robot.py
@@ -14,6 +14,9 @@ class Robot:
     self.socket = self.createSocket(self.PRIMARY_PORT)
     self.realTimeSocket = self.createSocket(self.REALTIME_PORT)
 
+    self.a = "1.0" # robots max acceleration
+    self.v = "0.5" # robots max velocity
+
   def createSocket(self, port):
     try:
       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,11 +32,81 @@ class Robot:
     URScript += new_line
     if self.socket:
       self.socket.sendall(URScript.encode('utf-8'))
-      print("test")
       return self.socket.recv(2048)  # number is the buffer size
     else:
-      print("Socket must be connected to send URScript.")
+      print("Socket must be connected to send URScript." )
 
-thisRobot = Robot()
-thisRobot.sendUrScript("movej([-1.57,-1.57,0, -1.57,0,0], a=1.0, v=0.5, r=0.01)") # home: [-1.57,-1.57,0, -1.57,0,0]
+  def moveJ(self, jointPositions: str):
+    self.sendUrScript(f"movej({jointPositions}, a={self.a}, v={self.v})")
+
+  def moveL():
+    {}
+  
+  # New Scale Precision Gripper
+  def openGripper(self):
+    print("open")
+    with open("urScripts\openGripper.script", "r") as file:
+      openScript = file.read()
+    self.sendUrScript(openScript)
+  
+  def closeGripper(self):
+    print("closing")
+    with open("urScripts\closeGripper.script", "r") as file:
+      closeScript = file.read()
+    self.sendUrScript(closeScript)
+
+  def receiveUserInput(self):
+    command = input("Enter newSquare (Example:'a1'):\n")
+    newSquare = list(command)
+
+    # go to IDLE before moving to squares or will cause joint errors
+    # Word commands in try loop
+    try: 
+      match command:
+        case "home":
+          self.moveJ("[-1.57,-1.57,0, -1.57,0,0]") # home
+        case "idle":
+          self.moveJ("[-1.5589281,-1.424189,0.959931, -1.15192,-1.6350244,0]") # idle
+        case "open":
+          self.openGripper()
+        case "close":
+          self.closeGripper()
+        # case "boot":
+        #   send_urscript_command("""
+        #     if safetymode() == 3:
+        #       power on
+        #       robotmode active 
+        #       unlock protective stop
+        #       brake release
+        #     end
+        #   #   """)
+        # case "pose":
+          # send_urscript_command("textmsg(get_actual_tcp_pose())")
+
+        case _:
+          raise Exception()
+        
+    except:
+      return
+      # Square commands in except loop
+      # check input validity
+      if newSquare[0] not in letters or newSquare[1] not in numbers:
+        print("Invalid Square, IDIOT.\n\n")
+      else:
+        print("Moving to:", newSquare, "\n\n")
+        calculateNewPos(newSquare)
+
+
+def main():
+  thisRobot = Robot()
+
+  while True:
+    thisRobot.receiveUserInput()
+
+main()
+
+
+
+
+
     
